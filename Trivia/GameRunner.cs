1121c6745f57ef0416cba1bf24c97c4723c5a558
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2022-12-06 10:25:18 +0100

    Posso iniettare Random in GameRunner

--- Trivia/GameRunner.cs
+++ Trivia/GameRunner.cs
@@ -12,17 +12,22 @@ namespace Trivia
 
         public static void Main(String[] args)
         {
+            Random rand = new Random();
+
+            Play(rand);
+        }
+
+        public static void Play(Random rand)
+        {
             Game aGame = new Game();
 
             aGame.add("Chet");
             aGame.add("Pat");
             aGame.add("Sue");
 
-            Random rand = new Random();
 
             do
             {
-
                 aGame.roll(rand.Next(5) + 1);
 
                 if (rand.Next(9) == 7)
@@ -33,14 +38,8 @@ namespace Trivia
                 {
                     notAWinner = aGame.wasCorrectlyAnswered();
                 }
-
-
-
             } while (notAWinner);
-
         }
-
-
     }
 
 }
