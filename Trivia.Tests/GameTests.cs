ded997ded4033494646a694e3f506b5c174bc9b7
simone.digiorgio <simone.digiorgio@wolterskluwer.com>
2022-12-14 12:27:13 +0100

    aggiunto test che verifica limite massimo giocatori

--- Trivia.Tests/GameTests.cs
+++ Trivia.Tests/GameTests.cs
@@ -55,4 +55,73 @@ Cannot play alone
         Assert.AreEqual(expected, stringWriter.ToString());
 
     }
+
+
+    [Test(Description = "Devo avere minimo due giocatori")]
+    public void DevoAvereMinimoDueGiocatori()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        var game = new Game();
+
+        game.add("Nando");
+        game.add("Steven");
+
+        game.roll(6);
+
+        var expected = @"Nando was added
+They are player number 1
+Steven was added
+They are player number 2
+Nando is the current player
+They have rolled a 6
+Nando's new location is 6
+The category is Sports
+Sports Question 0
+";
+
+
+        Assert.AreEqual(expected, stringWriter.ToString());
+
+    }
+
+
+    [Test(Description = "Posso giocare con al massimo 5 giocatori")]
+    public void PossoGiocareConAlMassimo5Giocatori()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        var game = new Game();
+
+        game.add("Steven1");
+        game.add("Steven2");
+        game.add("Steven3");
+        game.add("Steven4");
+        game.add("Steven5");
+
+        game.roll(6);
+
+        var expected = @"Steven1 was added
+They are player number 1
+Steven2 was added
+They are player number 2
+Steven3 was added
+They are player number 3
+Steven4 was added
+They are player number 4
+Steven5 was added
+They are player number 5
+Steven1 is the current player
+They have rolled a 6
+Steven1's new location is 6
+The category is Sports
+Sports Question 0
+";
+
+
+        Assert.AreEqual(expected, stringWriter.ToString());
+
+    }
 }
